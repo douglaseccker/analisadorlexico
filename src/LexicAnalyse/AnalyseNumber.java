f63c90fdf9782bf4f6ee8441f087e44dd0526bea
Douglas Eccker <douglaseccker@hotmail.com>
2018-04-19 19:44:49 -0300

    Ajustado analisadores

--- src/LexicAnalyse/AnalyseNumber.java
+++ src/LexicAnalyse/AnalyseNumber.java
@@ -7,19 +7,14 @@ public class AnalyseNumber extends AnalyseContract {
 		boolean found = true;
         boolean value = lexeme.matches("\\d{0,5}");
 
-        if (lexeme.charAt(0) == '0')
-        {
-            System.out.println("oi");
-        }
-
 		if (value == true) {
-			this.tokenName = "reserved_word";
+			this.tokenName = "number";
 			this.tokenValue = "int";
 		} else {
 			value = lexeme.matches("\\d+\\.\\d+");
 
 			if (value == true) {
-				this.tokenName = "reserved_word";
+				this.tokenName = "number";
 				this.tokenValue = "double";
 			} else {
 				found = false;
